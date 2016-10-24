fe23e11f9a6b2efca837ab30a7095ac1f1dd4ba5
Leon Cheung <funarchitect2022@gmail.com>
2016-10-24 15:18:43 -0700

    stashing

--- harpyie_web/classify/views.py
+++ harpyie_web/classify/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 
+from django.http import JsonResponse
+
 def index(request):
   return render(request, 'classify/index.html')
+
+def tag_spawn(request):
+  if request.method == 'POST':
+    return JsonResponse({ 'message' : 'success' })
+
+  return JsonResponse({ 'message', 'failure' })
